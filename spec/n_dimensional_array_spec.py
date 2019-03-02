a87737a916254f653f50f170e11b46eac35c8440
Sam Gathercole <sam@nextail.co>
2019-03-02 15:02:15 +0100

    N-dimensional array: constructing a 1-dimensional array

--- spec/n_dimensional_array_spec.py
+++ spec/n_dimensional_array_spec.py
@@ -3,5 +3,14 @@ from expects import *
 import numpy as np
 
 with description('N-dimensional array (ndarray)') as self:
-    with it('is a numpy.ndarray'):
-        expect(np.ndarray([])).to(be_a(np.ndarray))
+    with it('can be constructed with np.array'):
+        no_elements = []
+        expect(np.array(no_elements)).to(be_a(np.ndarray))
+
+    with it('can be a 1-dimensional array'):
+        elements = [1, 2, 3]
+
+        one_dimensional = np.array(elements)
+
+        expect(len(one_dimensional.shape)).to(equal(1))
+        expect(one_dimensional.shape[0]).to(equal(len(elements)))
