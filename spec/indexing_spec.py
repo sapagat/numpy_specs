fe79e93e2b4c956b40b88e7d132c4f817b33074f
Sam Gathercole <sam@nextail.co>
2019-03-02 20:00:37 +0100

    Indexing: slices an 2-dimensional matrix

--- spec/indexing_spec.py
+++ spec/indexing_spec.py
@@ -13,57 +13,108 @@ with description('Indexing') as self:
             stop = 7
             step = 2
 
-            slice = self.x[start:stop:step]
+            view = self.x[start:stop:step]
 
-            expect(slice).to(equal_np_array([1, 3, 5]))
+            expect(view).to(equal_np_array([1, 3, 5]))
 
         with it('uses by default a step equal to 1'):
             start = 2
             stop = 6
 
-            slice = self.x[start:stop]
+            view = self.x[start:stop]
 
-            expect(slice).to(equal_np_array([2, 3, 4, 5]))
+            expect(view).to(equal_np_array([2, 3, 4, 5]))
 
         with it('understands a negative start as "len(x) + start"'):
             start = -2
             stop = 10
 
-            slice = self.x[start:stop]
+            view = self.x[start:stop]
 
-            expect(slice).to(equal_np_array([8, 9]))
+            expect(view).to(equal_np_array([8, 9]))
 
         with it('understands a negative stop as "len(x) + stop"'):
             start = 2
             stop = -3
 
-            slice = self.x[start:stop]
+            view = self.x[start:stop]
 
-            expect(slice).to(equal_np_array([2, 3, 4, 5, 6]))
+            expect(view).to(equal_np_array([2, 3, 4, 5, 6]))
 
         with it('understands a negative step as going backwards'):
             start = 9
             stop = 1
             step = -2
 
-            slice = self.x[start:stop:step]
+            view = self.x[start:stop:step]
 
-            expect(slice).to(equal_np_array([9, 7, 5, 3]))
+            expect(view).to(equal_np_array([9, 7, 5, 3]))
 
         with it('allows to select all indices from start'):
             start = 7
 
-            slice = self.x[start:]
+            view = self.x[start:]
 
-            expect(slice).to(equal_np_array([7, 8, 9]))
+            expect(view).to(equal_np_array([7, 8, 9]))
 
         with it('allows to select all indices until a stop'):
             stop = 7
 
-            slice = self.x[:stop]
+            view = self.x[:stop]
 
-            expect(slice).to(equal_np_array([0, 1, 2, 3, 4, 5, 6]))
+            expect(view).to(equal_np_array([0, 1, 2, 3, 4, 5, 6]))
 
+    with description('working with a 2-dimensional matrix'):
+        with before.each:
+            self.matrix = np.array([
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ])
+
+        with it('allows to slice rows'):
+            view = self.matrix[0:2]
+
+            expect(view).to(equal_np_array([
+                [1, 2, 3],
+                [4, 5, 6]
+            ]))
+
+        with it('allows to slice columns'):
+            view = self.matrix[:, 1:]
+
+            expect(view).to(equal_np_array([
+                [2, 3],
+                [5, 6],
+                [8, 9]
+            ]))
+
+        with it('allows to slice both rows and columns'):
+            view = self.matrix[0:2, 1:]
+
+            expect(view).to(equal_np_array([
+                [2, 3],
+                [5, 6]
+            ]))
+
+        with it('has some suprises when chaining slices'):
+            view = self.matrix[0:2, 1:]
+            alternative = self.matrix[0:2][1:]
+
+            expect(view).not_to(equal_np_array(alternative))
+            expect(alternative).to(equal_np_array([
+                [4, 5, 6]
+            ]))
+
+        with it('can add a new dimension'):
+            view = self.matrix[:, np.newaxis, :]
+
+            expect(view.shape).to(equal((3, 1, 3)))
+            expect(view).to(equal_np_array([
+                [[1, 2, 3]],
+                [[4, 5 ,6]],
+                [[7, 8, 9]]
+            ]))
 
 class equal_np_array(Matcher):
     def __init__(self, expected):
